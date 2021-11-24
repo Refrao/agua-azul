367ec60ddb9fef3072c4cf3a5b8acd2f4cda0a60
Refrao <94881413+Refrao@users.noreply.github.com>
2021-11-24 09:28:02 -0300

    v2

--- Corte - historico execucao.sql
+++ Corte - historico execucao.sql
@@ -1,7 +1,16 @@
-SELECT C.numero_cliente,to_ddmmyyyy(date(C.fecha_corte)) as fecha_corte, to_ddmmyyyy(date(C.data_solic_corte)) as data_solic_corte, b.descripcion as motivo_corte,a.descricao_tipo as descricao_tipo, f.descripcion as retorno_corte, to_ddmmyyyy(date(c.fecha_reposicion)) as fecha_reposicion, d.descripcion as motivo_relig, e.descripcion as retorno_relig, COUNT(*), AVG(valor_divida)
+SELECT C.numero_cliente,
+to_ddmmyyyy(date(C.fecha_corte)) as fecha_corte,
+to_ddmmyyyy(date(C.data_solic_corte)) as data_solic_corte,
+b.descripcion as motivo_corte,
+a.descricao_tipo as descricao_tipo,
+f.descripcion as retorno_corte, 
+to_ddmmyyyy(date(c.fecha_reposicion)) as fecha_reposicion,
+d.descripcion as motivo_relig,
+e.descripcion as retorno_relig, 
+COUNT(*), 
+AVG(valor_divida)
 FROM CORREP C, outer tabla b, outer tabla f, outer tabla d, outer tipo_corte a, outer tabla e
-WHERE
-b.nomtabla = 'CORMOT' And b.codigo = c.motivo_corte AND b.sucursal = '0000'  And ((b.fecha_activacion >= b.fecha_desactivac And b.fecha_desactivac > Today)  Or b.fecha_desactivac is null) 
+WHERE b.nomtabla = 'CORMOT' And b.codigo = c.motivo_corte AND b.sucursal = '0000'  And ((b.fecha_activacion >= b.fecha_desactivac And b.fecha_desactivac > Today)  Or b.fecha_desactivac is null) 
 AND f.nomtabla = 'ACRECO' AND f.codigo = c.acc_realizada_cor AND b.sucursal = '0000'  And ((f.fecha_activacion >= f.fecha_desactivac And f.fecha_desactivac > Today)  Or  f.fecha_desactivac is null) 
 AND d.nomtabla = 'REPMOT' AND d.codigo = c.motivo_repo And d.sucursal = '0000' And ((d.fecha_activacion >= d.fecha_desactivac And d.fecha_desactivac > Today)  Or d.fecha_desactivac is null) 
 AND e.nomtabla = 'ACRERE' AND e.codigo = c.acc_realizada_rep And e.sucursal = '0000' And ((e.fecha_activacion >= e.fecha_desactivac And e.fecha_desactivac > Today)  Or e.fecha_desactivac is null)
